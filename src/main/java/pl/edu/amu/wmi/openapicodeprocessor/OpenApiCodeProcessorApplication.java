6352ac611f4712b88fc757e657d833bdfd4298b6
Eryk Mariankowski <dagi12@o2.pl>
2021-09-01 13:14:09 +0200

    first commit

---
+++ src/main/java/pl/edu/amu/wmi/openapicodeprocessor/OpenApiCodeProcessorApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.amu.wmi.openapicodeprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiCodeProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiCodeProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/amu/wmi/openapicodeprocessor/OpenApiCodeProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.amu.wmi.openapicodeprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiCodeProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
